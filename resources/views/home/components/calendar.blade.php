267d2c1bf5425edf0d636b72eb21bae388bcde77
Joao <newemo14@gmail.com>
2021-08-16 14:40:35 +0100

    Fix home.index blade

--- resources/views/home/components/calendar.blade.php
+++ resources/views/home/components/calendar.blade.php
@@ -18,7 +18,7 @@
                         "</br> <b>Organizador:</b> {{ $data->host_name }}" +
                         `</br> <b>Descricao:</b> {{ $data->description }}`,
                     date: "{{ $formatDate->formatDate($data->event_start_date) }}", // Event date (required)
-                    color: "#63d867", // Event custom color (optional)
+                    color: @if(date("Y-m-d") > $data->event_end_date) "#63d867" @else "#FF0000" @endif, // Event custom color (optional)
                 },
                 @endforeach
             ]
