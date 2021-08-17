7b088acb1786265212ee358f7e9804b4da0d185d
Joao <newemo14@gmail.com>
2021-08-17 09:52:56 +0100

    fix heroku layout

--- resources/views/home/scriptsFooter.blade.php
+++ resources/views/home/scriptsFooter.blade.php
@@ -4,6 +4,10 @@
 <script type="text/javascript">
 
     $(document).ready(function () {
+        $('.phone').mask('(00) 0 0000-0000')
 
+        $('#btn-add-comp').click(function (event) {
+            $('#add-tournament-date-modal').modal('show');
+        });
     });
 </script>
